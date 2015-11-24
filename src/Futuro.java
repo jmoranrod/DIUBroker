0d987800c4b562cd85dba90b77f059c0c6e8b6e4
Javier <jmoranrod@gmail.com>
2015-11-24 09:13:17 +0000

    Project Created.

---
+++ src/Futuro.java
@@ -0,0 +1,27 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Juan
+ */
+
+public class Futuro {
+    public String Hora;
+    public String Volumen;
+    public String Ultimo;
+    public String Compra_Vol;
+    public String Compra_Precio;
+    public String Venta_Vol;
+    public String Venta_Precio;
+    public String Vencimiento;
+    public String Apertura;
+    public String Anterior;
+    public String Maximo;
+    public String Minimo;
+}
---
+++ src/Opcion.java
@@ -0,0 +1,27 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Juan
+ */
+public class Opcion {
+    public String Tipo;
+    public String Hora;
+    public String Volumen;
+    public String Ultimo;
+    public String Compra_Vol;
+    public String Compra_Precio;
+    public String Venta_Vol;
+    public String Venta_Precio;
+    public String Vencimiento;
+    public String Ejercicio;
+}
+
+
+
